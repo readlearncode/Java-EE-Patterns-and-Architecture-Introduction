a975e7e4067f348db11e79f04f4efb8c33a502ff
Alex Theedom <alex.theedom@gmail.com>
2017-11-15 19:28:48 +0100

    add filter example

---
+++ master/src/main/java/com/readlearncode/filter/AuthenticationFilter.java
@@ -0,0 +1,30 @@
+package com.readlearncode.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@WebFilter(filterName = "authentication", urlPatterns = "/admins/*", servletNames = "AdminRequestHandler")
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        if (httpRequest.getSession().getAttribute("USER_TYPE").equals("ADMIN")) {
+            chain.doFilter(httpRequest, response);
+        } else {
+            ((HttpServletResponse) response).sendRedirect("/admin-login.html");
+        }
+
+    }
+}
