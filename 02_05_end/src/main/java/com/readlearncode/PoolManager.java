719384638016eea0fa4872b704c1b946b4e0192c
Alex Theedom <alex.theedom@gmail.com>
2017-12-04 12:32:57 -0800

    add 02_05 start and end states

---
+++ 02_05_end/src/main/java/com/readlearncode/PoolManager.java
@@ -0,0 +1,41 @@
+package com.readlearncode;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.*;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
+@DependsOn("Configuration")
+@Startup
+@Singleton
+public class PoolManager {
+
+    private Queue<Object> pooledObjects;
+
+    @PostConstruct
+    void constructExpensiveObjects() {
+        pooledObjects = new LinkedBlockingQueue<>(1_000);
+        for (int i = 0; i <= 1_000; i++) {
+            pooledObjects.offer(new Object());
+        }
+    }
+
+    @AccessTimeout(value = 30, unit = TimeUnit.SECONDS)
+    @Lock(LockType.WRITE)
+    public void returnObject(Object obj) {
+        pooledObjects.offer(obj);
+    }
+
+    @Lock(LockType.READ)
+    public Object borrowObject() {
+        return pooledObjects.poll();
+    }
+}
---
+++ 02_05_start/src/main/java/com/readlearncode/Configuration.java
@@ -0,0 +1,15 @@
+package com.readlearncode;
+
+import javax.ejb.Startup;
+import javax.inject.Singleton;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Startup
+@Singleton
+public class Configuration {
+}
