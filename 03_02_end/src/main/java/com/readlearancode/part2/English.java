9e3ab01fe55fea21d242ff44a3424f298d284ef5
Alex Theedom <alex.theedom@gmail.com>
2018-03-05 14:40:09 -0800

    add 03_02 start/end state

---
+++ 03_02_end/src/main/java/com/readlearancode/part2/English.java
@@ -0,0 +1,10 @@
+package com.readlearancode.part2;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class English implements Subject {
+}
---
+++ 03_02_end/src/main/java/com/readlearancode/part2/Target.java
@@ -0,0 +1,17 @@
+package com.readlearancode.part2;
+
+import javax.inject.Inject;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class Target {
+
+    @Inject
+    @Favourite
+    private Subject subject;
+
+}
